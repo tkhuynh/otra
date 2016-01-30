a6130e7be2fb70bc3bb0ae062af6c0dfd2851523
abeytaadam <measalion@gmail.com>
2016-01-29 16:25:39 -0800

    jQuery remove form fields and prevent removed from saving to db

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,5 +1,5 @@
 class Tour < ActiveRecord::Base
 	belongs_to :band
-	has_many :performances
-	accepts_nested_attributes_for :performances
+	has_many :performances, dependent: :destroy
+	accepts_nested_attributes_for :performances, reject_if: lambda { |a| a[:performance_date] or a[:location].blank? }, allow_destroy: true
 end
